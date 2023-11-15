35a6ecb4c18501a9e4ad968d96d9f55042f46fa3
andrewbrook <andrewbrook@qq.com>
2023-11-15 14:51:37 +0800

    watch监视ref数据的说明

---
+++ 09_src_watch监视ref数据的说明/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- vue3组件中的模板结构可以没有根标签 -->
+  <Demo/>
+</template>
+
+<script>
+  import Demo from './components/Demo'
+  export default {
+    name: 'App',
+    components:{Demo},
+  }
+</script>
+
+<style>
+
+</style>
